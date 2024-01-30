b2237a18abd5dd13672b1dbd432c6e627b19d916
evilimas <eg.vilimas@gmail.com>
2024-01-30 09:57:08 +0100

    reworked / fixed

--- script.js
+++ script.js
@@ -1,49 +1,128 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+];
 
-
-let passwordLength = 15
+let passwordLength = 15;
 
 //generate random character
-function generateRandomCharecters(){
-    return characters[Math.floor(Math.random() * characters.length)]
+function generateRandomCharecters() {
+  return characters[Math.floor(Math.random() * characters.length)];
 }
 
 //generate random password from random characters
-function generateRandomPasswords(){
-    let randomPasswords = ""
-    for(let i = 0; i < passwordLength; i++){
-        randomPasswords += generateRandomCharecters()
-    }
-    return randomPasswords
+function generateRandomPasswords() {
+  let randomPasswords = '';
+  for (let i = 0; i < passwordLength; i++) {
+    randomPasswords += generateRandomCharecters();
+  }
+  return randomPasswords;
 }
-
 
 //render input fields with random password when clicked the button
 
-let generateBtn = document.getElementById("btn")
-let inputOne = document.getElementById("input-one")
-let inputTwo = document.getElementById("input-two")
+const generateBtn = document.getElementById('btn');
+const passwordOne = document.getElementById('password-one');
+const passwordTwo = document.getElementById('password-two');
 
-
-
-generateBtn.addEventListener("click", function(){
-    let generateRandomPasswordOne = generateRandomPasswords()
-    let generateRandomPasswordTwo = generateRandomPasswords()
-
-    inputOne.value = generateRandomPasswordOne
-    inputTwo.value = generateRandomPasswordTwo
-   
-})
+generateBtn.addEventListener('click', function () {
+  passwordOne.textContent = generateRandomPasswords();
+  passwordTwo.textContent = generateRandomPasswords();
+});
 // copyn selected input
 
-inputOne.addEventListener("click", function(){
-    this.select()
-    document.execCommand("copy")
-    
-})
-inputTwo.addEventListener("click", function(){
-    this.select()
-    document.execCommand("copy")
-    
-})
+passwordOne.addEventListener('click', function () {
+  navigator.clipboard.writeText(passwordOne.textContent);
+});
+passwordTwo.addEventListener('click', function () {
+  navigator.clipboard.writeText(passwordTwo.textContent);
+});
